cf9dd6af61c725d210a11f37f686dc428f62bccb
Marcelo Martins <marcelo.a.martins@hotmail.com>
2020-06-21 00:03:13 -0300

    Update

--- venv/app.py
+++ venv/app.py
@@ -4,7 +4,7 @@ from flask import Flask
    
 def create_app():
     app = Flask(__name__)
-    
+    views.init_app(app)
     return app
     
     
--- venv/views.py
+++ venv/views.py
@@ -1,3 +1,5 @@
+
+
 def init_app(app):
 
     @app.route('/')
